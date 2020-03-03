9c41835f62af3b3ea3e1803cd4c66e83c9a4aeb4
Ichizo Umehara <iumehara10@gmail.com>
2020-03-03 12:12:06 +0900

    Add corsica

--- api/lib/chatter_web/endpoint.ex
+++ api/lib/chatter_web/endpoint.ex
@@ -41,5 +41,6 @@ defmodule ChatterWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Corsica, origins: "http://localhost:3000"
   plug ChatterWeb.Router
 end

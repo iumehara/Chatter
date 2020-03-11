911617779fccf895c0a286ba68c0d87679b77e08
Ichizo Umehara <iumehara10@gmail.com>
2020-03-11 10:41:55 +0900

    Add DB setup to ci

--- api/config/test.exs
+++ api/config/test.exs
@@ -5,7 +5,7 @@ config :chatter, Chatter.Repo,
   username: "postgres",
   password: "postgres",
   database: "chatter_test",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
